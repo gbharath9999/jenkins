ca032c97e4e806cbde76c74a73e3298e61d7f556
bharath <gbharath9999@gmail.com>
2018-04-06 15:08:50 +0530

    this is commited by bharath

---
+++ src/test/java/chiselon/jerkins/Test_1.java
@@ -0,0 +1,14 @@
+package chiselon.jerkins;
+
+import org.testng.annotations.Test;
+
+public class Test_1 {
+
+	
+	
+	@Test
+	public void m1(){
+		
+		System.out.println("hello .......:)");
+	}
+}
---
+++ src/test/java/com/chiselon/jenkins1/Test_2.java
@@ -0,0 +1,10 @@
+package com.chiselon.jenkins1;
+
+public class Test_2 {
+	
+	public void m2() {
+		
+		System.out.println("hi am m2 method");
+	}
+
+}
